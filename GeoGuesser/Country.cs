43e7be65848ff15b404de044209f6dd8e00b91ae
SriramLol <69857603+SriramLol@users.noreply.github.com>
2023-05-10 09:54:07 -0400

    Update Country.cs

--- GeoGuesser/Country.cs
+++ GeoGuesser/Country.cs
@@ -26,26 +26,47 @@ public void fillCountries(String s, String url){
 public void champie(){
 
 
-fillCountries("China", );
-fillCountries ("United States of America", );
-fillCountries("Japan", );
-fillCountries("Algeria", );
-fillCountries("Albania", );
-fillCountries("Andorra", );
-fillCountries("Angola", );
-fillCountries("Argentina", );
-fillCountries("Bahamas", );
-fillCountries("Bangladesh", );
-fillCountries("Bahrain", );
-fillCountries("Belgium", );
-fillCountries("Belize", );
-fillCountries("Benin", );
-fillCountries("Bulguria", );
-fillCountries("Canada", );
-fillCountries("Cambodia, );
-fillCountries("Chile", );
-fillCountries("Costa Rica", );
-fillCountries("Croatia", );
+fillCountries("China","https://www.burningcompass.com/countries/china/maps/china-outline-map.jpg");
+
+fillCountries ("United States of America","https://media.istockphoto.com/id/835357658/vector/usa-map.jpg?s=612x612&w=0&k=20&c=2NWn9P2Zdcfn73WIESjUvoc0rWJaDVP24ZhT6hkcBzw=" );
+
+fillCountries("Japan","https://vemaps.com/uploads/img/big/jp-01.jpg" );
+
+fillCountries("Algeria","https://ecdn.teacherspayteachers.com/thumbitem/Algeria-Map-Black-White-Solid-Outline-Algerian-Map-JEPG-SVG-PNG-PDF-EPS-AI-6424112-1610064289/original-6424112-3.jpg" );
+
+fillCountries("Albania","https://vemaps.com/uploads/img/big/al-01.jpg");
+
+fillCountries("Andorra","https://www.burningcompass.com/countries/andorra/maps/andorra-outline-map-hd.jpg" );
+
+fillCountries("Angola","https://i.pinimg.com/736x/bc/29/37/bc2937e9610ba2316f4a33fc9b273665.jpg");
+
+fillCountries("Argentina","https://static.vecteezy.com/system/resources/previews/002/905/377/non_2x/outline-simple-map-of-argentina-free-vector.jpg");
+
+fillCountries("Bahamas","https://static.vecteezy.com/system/resources/previews/003/087/757/original/outline-simple-map-of-bahamas-free-vector.jpg");
+
+fillCountries("Bangladesh","https://i.pinimg.com/736x/45/d1/dc/45d1dc021cc9f1587956326b10652424.jpg");
+
+fillCountries("Bahrain","https://ecdn.teacherspayteachers.com/thumbitem/Bahrain-Map-Black-White-Solid-Outline-Country-Map-JPG-SVG-PNG-PDF-EPS-AI-6424159-1610065626/original-6424159-3.jpg");
+
+fillCountries("Belgium","https://www.burningcompass.com/countries/belgium/maps/belgium-outline-map.jpg");
+
+fillCountries("Belize","https://thumbs.dreamstime.com/b/belize-black-contour-map-outline-country-207853861.jpg");
+
+fillCountries("Benin","https://vemaps.com/uploads/img/big/bj-01.jpg");
+
+fillCountries("Bulgaria","https://www.burningcompass.com/countries/bulgaria/maps/bulgaria-outline-map-hd.jpg");
+
+fillCountries("Canada","https://thumbs.dreamstime.com/b/map-canada-icon-outline-style-illustration-vector-web-86231782.jpg");
+
+fillCountries("Cambodia","https://www.burningcompass.com/countries/cambodia/maps/cambodia-outline-map.jpg");
+
+fillCountries("Chile","https://previews.123rf.com/images/skvoor/skvoor0712/skvoor071200236/2229683-chile-outline-map-with-shadow-detailed-mercator-projection.jpg");
+
+fillCountries("Costa Rica","https://www.worldatlas.com/r/w768/webimage/countrys/namerica/camerica/outline/croutl.gif" );
+
+fillCountries("Croatia","https://vemaps.com/uploads/img/hr-01.png" );
+
+/*
 fillCountries("Cuba", );
 fillCountries("Djibouti", );
 fillCountries("Dominica", );
@@ -127,6 +148,7 @@ fillCountries("Rwanda", );
 fillCountries("Saint Lucia", );
 fillCountries("Senegal", );
 fillCountries("Serbia", );
+*/
 }
 }
 
