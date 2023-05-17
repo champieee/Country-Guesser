fa9a3617a64e4eb3853e0e94b598dad84080b3c3
ayush <asingh5043@inst.hcpss.org>
2023-05-17 09:23:24 -0400

    randomizers

--- GeoGuesser/Form1.cs
+++ GeoGuesser/Form1.cs
@@ -16,17 +16,18 @@ namespace GeoGuesser
         Dictionary<int, Array> countries = new Dictionary<int, Array>();
         Dictionary<int, HashSet<string>> keyValuePairs= new Dictionary<int, HashSet<string>>();
         int score = 0;
-        int country;
+        int country; 
         Random rnd = new Random();
+        Boolean found;  
         
 
         public Form1()
         {
-            country = rnd.Next(0, 2);
+            country = rnd.Next(0, 99);
             c1.fillCountries();
             keyValuePairs = c1.getCountries(); 
             InitializeComponent();
-            string imageLink = keyValuePairs[country].ToArray()[country]; 
+            string imageLink = keyValuePairs[country].ToArray()[1]; 
             mapBox.SizeMode = PictureBoxSizeMode.StretchImage;
             mapBox.ImageLocation = imageLink;
         }
@@ -36,11 +37,15 @@ namespace GeoGuesser
         {
             g1.getGuess(userText.Text);
 
-            if (userText.Text.Equals(keyValuePairs[country].ToArray()[country]))
+            if (userText.Text.Equals(keyValuePairs[country].ToArray()[0]))
             {
                 score++; 
                 scoreLbl.Text = "Score: " + score;
                 userText.Clear();
+                country = rnd.Next(0, 99);
+                string imageLink = keyValuePairs[country].ToArray()[1];
+                mapBox.SizeMode = PictureBoxSizeMode.StretchImage;
+                mapBox.ImageLocation = imageLink;
             }
         }
     }
